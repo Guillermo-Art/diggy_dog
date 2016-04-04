5b2a17e3aadde6c46e6a54cd9879b595b96927b1
Guillermo-Art <guillermoart132@gmail.com>
2016-04-04 15:34:20 -0500

    Initial commit

---
+++ app/src/main/java/com/sourcey/materiallogindemo/Utils/AppConstants.java
@@ -0,0 +1,13 @@
+package com.sourcey.materiallogindemo.Utils;
+
+import com.kii.cloud.storage.Kii.Site;
+/**
+ * Created by GuillermoArturo on 04/04/2016.
+ */
+public class AppConstants {
+
+    public static final String APP_ID = "c26400d4";
+    public static final String APP_KEY = "0479f0e74052e21e2fbd1dc60284f025";
+    public static final Site APP_SITE = Site.US;
+    public static final String APP_BUCKET_NAME = "usuarios";
+}
